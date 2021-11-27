adbfabba384a81e21f1499e04eddcf323cc47aac
DanielShamir93 <daniel.shamir.93@gmail.com>
2021-11-27 11:21:02 +0200

    starting adding js

---
+++ js/index.js
@@ -0,0 +1,11 @@
+window.onload = () => {
+    const parentClassName = 'carousel-content';
+    for (item in items) {
+        appendItem(item);
+    }
+}
+
+
+const appendItem = (parentClassName, item) => {
+
+}
---
+++ js/variables.js
@@ -0,0 +1,10 @@
+const items = [
+
+    {
+        src: 'url(../assets/images/713mKTazWmL-_AC_UL160_SR160-160_.jpg)',
+        title: 'Binary Counting As Easy As 01 10 10 For IT Nerd T-Shirt',
+        price: 13.98,
+        shipping: 20.50
+    },
+
+];
