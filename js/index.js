ed9b436c7943c56d5570a7fa44d0460c9b63405f
DanielShamir93 <daniel.shamir.93@gmail.com>
2021-11-27 14:40:08 +0200

    carousel layout done without JS

--- js/index.js
+++ js/index.js
@@ -1,11 +1,45 @@
-window.onload = () => {
-    const parentClassName = 'carousel-content';
-    for (item in items) {
-        appendItem(item);
+
+const getCarouselItems = () => {
+    const carousel = document.querySelector('.carousel-content');
+    for (let i = 0; i < MAX_CAROUSEL_SIZE; i++) {
+        appendItemToCarousel(carousel, items[i]);
     }
 }
 
-
-const appendItem = (parentClassName, item) => {
-
+const appendItemToCarousel = (carousel, item) => {
+    const div = document.createElement('div');
+    div.className = 'carousel-content-item';
+    div.innerHTML = toInnerHTML(item);
+    carousel.appendChild(div);
 }
+
+// Convert item object to innerHTML
+const toInnerHTML = (item) => {
+    let rating = '';
+
+    // item was rated
+    if (item.stars !== 0) {
+        rating = `
+            <div class="item-rating">
+                <figure class="item-stars" style="${item.stars.style}"></figure>
+                <span class="item-global-rating">${item.global_rating}</span>
+            </div>
+        `;
+    }
+
+    return (`
+        <div class="item-img-container">
+            <img class="item-img" src="${item.src}" alt="shirt-img"/>
+        </div>
+        <p class="item-title">${item.title}</p>
+        ${rating}
+        <p class="item-price"><span>$</span>${item.price}</p>
+        <p class="item-shipping-price">$${item.shipping_price} shipping</p>
+    `)
+}
+
+
+getCarouselItems();
+
+
+
--- js/variables.js
+++ js/variables.js
@@ -1,10 +1,203 @@
+const MAX_CAROUSEL_SIZE = 8;
+
+const stars = {
+    0.5:    { style: 'background-position: -278px -120px;' },
+    1:      { style: 'background-position: -390px -230px;' },
+    1.5:    { style: 'background-position: -293px -206px;' },
+    2:      { style: 'background-position: -181px -76px;' },
+    2.5:    { style: 'background-position: -99px -68px;' },
+    3:      { style: 'background-position: -84px -48px;' },
+    3.5:    { style: 'background-position: -181px -56px;' },
+    4:      { style: 'background-position: -84px -8px;' },
+    4.5:    { style: 'background-position: -84px -28px;' },
+    5:      { style: 'background-position: -166px -36px;' },
+}
+
 const items = [
-
     {
-        src: 'url(../assets/images/713mKTazWmL-_AC_UL160_SR160-160_.jpg)',
-        title: 'Binary Counting As Easy As 01 10 10 For IT Nerd T-Shirt',
+        src: './assets/images/items/31ATAlUkMIL-_AC_SR160-200_.jpg',
+        title: 'I\'m thinking - T-Shirt',
+        stars: stars[3.5],
+        global_rating: 0,
         price: 13.98,
-        shipping: 20.50
+        shipping_price: 20.50
     },
-
+    {
+        src: './assets/images/items/31KozAwP69L-_AC_SR160-200_.jpg',
+        title: 'sudo apt get install - T-Shirt',
+        stars: stars[5],
+        global_rating: 1,
+        price: 0,
+        shipping_price: 0
+    },
+    {
+        src: './assets/images/items/31KSPlqgNdL-_AC_SR160-200_.jpg',
+        title: '/dev/null - T-Shirt',
+        stars: stars[5],
+        global_rating: 1,
+        price: 0,
+        shipping_price: 0
+    },
+    {
+        src: './assets/images/items/31lRIJJAAgL-_AC_SR160-200_.jpg',
+        title: 'There is no cloud - T-Shirt',
+        stars: stars[5],
+        global_rating: 1,
+        price: 0,
+        shipping_price: 0
+    },
+    {
+        src: './assets/images/items/31s2OPfSNGL-_AC_SR160-200_.jpg',
+        title: 'There\'s no place like 127.0.0.1 - T-Shirt',
+        stars: stars[5],
+        global_rating: 1,
+        price: 0,
+        shipping_price: 0
+    },
+    {
+        src: './assets/images/items/31w6K06sUeL-_AC_SR160-200_.jpg',
+        title: '/dev/null white - T-Shirt',
+        stars: stars[5],
+        global_rating: 1,
+        price: 0,
+        shipping_price: 0
+    },
+    {
+        src: './assets/images/items/31XWEaFekdS-_AC_SR160-200_.jpg',
+        title: 'ascii symbols - T-Shirt',
+        stars: stars[5],
+        global_rating: 1,
+        price: 0,
+        shipping_price: 0
+    },
+    {
+        src: './assets/images/items/41-8DC43a4L-_AC_SR160-200_.jpg',
+        title: 'Running code is my cardio - T-Shirt',
+        stars: stars[5],
+        global_rating: 1,
+        price: 0,
+        shipping_price: 0
+    },
+    {
+        src: './assets/images/items/41aBXsQ-tAL-_AC_SR160-200_.jpg',
+        title: 'Devops even sysadmins need heros - T-Shirt',
+        stars: stars[5],
+        global_rating: 1,
+        price: 0,
+        shipping_price: 0
+    },
+    {
+        src: './assets/images/items/41bp-u7XlRL-_AC_SR160-200_.jpg',
+        title: 'Happy sysadmin - T-Shirt',
+        stars: stars[5],
+        global_rating: 1,
+        price: 0,
+        shipping_price: 0
+    },
+    {
+        src: './assets/images/items/41J-1EFqrkL-_AC_SR160-200_.jpg',
+        title: 'Sysadmins because even developers need heros - T-Shirt',
+        stars: stars[5],
+        global_rating: 1,
+        price: 0,
+        shipping_price: 0
+    },
+    {
+        src: './assets/images/items/41J6PrPd76L-_AC_SR160-200_.jpg',
+        title: 'sudo apt-get install coffee - T-Shirt',
+        stars: stars[5],
+        global_rating: 1,
+        price: 0,
+        shipping_price: 0
+    },
+    {
+        src: './assets/images/items/61bvMLhdamS-_AC_UL160_SR160-160_.jpg',
+        title: 'I am hero because you built something - T-Shirt',
+        stars: stars[5],
+        global_rating: 1,
+        price: 0,
+        shipping_price: 0
+    },
+    {
+        src: './assets/images/items/61mUuuMjcES-_AC_UL160_SR160-160_.jpg',
+        title: 'code - T-Shirt',
+        stars: stars[5],
+        global_rating: 1,
+        price: 0,
+        shipping_price: 0
+    },
+    {
+        src: './assets/images/items/61u08eELvVS-_AC_UL160_SR160-160_.jpg',
+        title: 'You destroyed me - T-Shirt',
+        stars: stars[5],
+        global_rating: 1,
+        price: 0,
+        shipping_price: 0
+    },
+    {
+        src: './assets/images/items/71emPiYYJXL-_AC_UL160_SR160-160_.jpg',
+        title: 'There\'s no place like 127.0.0.1 - T-Shirt',
+        stars: stars[5],
+        global_rating: 1,
+        price: 0,
+        shipping_price: 0
+    },
+    {
+        src: './assets/images/items/71JLgApJDrL-_AC_UL160_SR160-160_.jpg',
+        title: 'Engineer - T-Shirt',
+        stars: stars[5],
+        global_rating: 1,
+        price: 0,
+        shipping_price: 0
+    },
+    {
+        src: './assets/images/items/71Of9eTD6mL-_AC_UL160_SR160-160_.jpg',
+        title: 'LT.Pro - T-Shirt',
+        stars: stars[5],
+        global_rating: 1,
+        price: 0,
+        shipping_price: 0
+    },
+    {
+        src: './assets/images/items/71r7q0fv10L-_AC_UL160_SR160-160_.jpg',
+        title: 'Code joke - T-Shirt',
+        stars: stars[5],
+        global_rating: 1,
+        price: 0,
+        shipping_price: 0
+    },
+    {
+        src: './assets/images/items/71SuDHMZ5gL._AC_UL480_SR480,480_.jpg',
+        title: 'Programmer will ctrl-save the world - T-Shirt',
+        stars: stars[5],
+        global_rating: 1,
+        price: 0,
+        shipping_price: 0
+    },
+    {
+        src: './assets/images/items/71tcB8XdZ8L-_AC_UL160_SR160-160_.jpg',
+        title: 'Hacker - T-Shirt',
+        stars: stars[5],
+        global_rating: 1,
+        price: 0,
+        shipping_price: 0
+    },
+    {
+        src: './assets/images/items/313gwi00WtL-_AC_SR160-200_.jpg',
+        title: 'Sudo apt rm -rf - T-Shirt',
+        stars: stars[5],
+        global_rating: 1,
+        price: 0,
+        shipping_price: 0
+    },
+    {
+        src: './assets/images/items/713mKTazWmL._AC_UL320_SR320,320_.jpg',
+        title: 'Binary counting - T-Shirt',
+        stars: stars[5],
+        global_rating: 1,
+        price: 0,
+        shipping_price: 0
+    },
 ];
+
+
